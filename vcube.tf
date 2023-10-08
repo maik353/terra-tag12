e3b9571f865c74685e75ecd2949c5f979932b0ef
maik353 <132044806+maik353@users.noreply.github.com>
2023-10-08 17:14:38 +0530

    Update vcube.tf

--- vcube.tf
+++ vcube.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA34WFJYOIZBTJ6WF4"
-  secret_key = "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"
+  region     = "eu-north-1"
+  access_key = "AKIAZG3FFTUH72ORCZMR"
+  secret_key = "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"
 }
 
 # Create a bucket
 resource "aws_s3_bucket" "b1" {
 
-  bucket = "s3-terraform-bucket-lab-mahesh"
+  bucket = "s3-terraform-bucket-lab-mahesh1"
 
   acl    = "private"   # or can be "public-read"
 
